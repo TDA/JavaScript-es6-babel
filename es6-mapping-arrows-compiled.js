d112e3114660c5e183ab8ef7f17339ddc5da2a0f
Sai Chandramouli <schand31@asu.edu>
2016-06-04 23:37:02 -0700

    set up babel with webstorm woohoo

--- es6-mapping-arrows-compiled.js
+++ es6-mapping-arrows-compiled.js
@@ -11,4 +11,12 @@ console.log([1, 2, 3].map(function (n) {
   return n * n;
 }));
 
+var evens = [1, 2, 3, 4];
+var nums = evens.map(function (v, i) {
+  return v + i;
+});
+console.log(nums);
+
+console.log(nums);
+
 //# sourceMappingURL=es6-mapping-arrows-compiled.js.map
--- es6-mapping-arrows.js
+++ es6-mapping-arrows.js
@@ -4,3 +4,9 @@
 
 console.log([1, 2, 3].map(n => n + 1));
 console.log([1, 2, 3].map(n => n * n));
+
+var evens = [1, 2, 3, 4];
+var nums = evens.map((v, i) => v + i);
+console.log(nums);
+
+console.log(nums);
