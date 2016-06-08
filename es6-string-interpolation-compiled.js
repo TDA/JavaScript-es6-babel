a2d0beb55be50f803b9a8045ba31c2b8c2eb3b52
Sai Chandramouli <schand31@asu.edu>
2016-06-08 00:30:13 -0700

    multiline strings

--- es6-string-interpolation-compiled.js
+++ es6-string-interpolation-compiled.js
@@ -9,4 +9,8 @@ var age = 22;
 
 console.log(name + ' is ' + age + ' years old :)');
 
+var multiline_string = 'This is a multi-line string\nwhich spans across lines\nand still executes\nwith the newlines intact\nkinda like pythons """, but\nwith <pre> kinda stuff\n';
+
+console.log(multiline_string);
+
 //# sourceMappingURL=es6-string-interpolation-compiled.js.map
--- es6-string-interpolation.js
+++ es6-string-interpolation.js
@@ -6,3 +6,13 @@ var name = 'sai';
 var age = 22;
 
 console.log(`${name} is ${age} years old :)`);
+
+var multiline_string = `This is a multi-line string
+which spans across lines
+and still executes
+with the newlines intact
+kinda like pythons """, but
+with <pre> kinda stuff
+`;
+
+console.log(multiline_string);
